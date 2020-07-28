ac3ef5610190a75c70b1f34d0baa7d674ce24bb1
murilokuerten92 <murilokuerten92@gmail.com>
2020-07-28 18:45:15 -0300

    news features

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import cadastroVideos from './pages/cadastro/Videos';
 import Home from './pages/Home';
+import cadastroCategoria from './pages/cadastro/Categoria';
 
 
 const pagina404 = () => (<div>Erro Pagina 404</div>)
@@ -11,8 +12,9 @@ const pagina404 = () => (<div>Erro Pagina 404</div>)
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-    <Route path='/' exact component={Home} />
-      <Route path='/cadastro/video' component={cadastroVideos} />   
+      <Route path='/' exact component={Home} />
+      <Route path='/cadastro/video' component={cadastroVideos} />
+      <Route path='/cadastro/categoria' component={cadastroCategoria} />
       <Route component={pagina404} />
     </Switch>
   </BrowserRouter>,
